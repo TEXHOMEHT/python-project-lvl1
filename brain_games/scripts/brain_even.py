861f3edf5a2b8e423bd19f2b88ca4034e74f0c98
gregory.s <73caramba@gmail.com>
2022-08-28 15:29:31 +0300

    Reworked brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,35 +1,10 @@
 #!/usr/bin/env python3
-from random import randint
+from brain_games.parity_check import parity_check
 
 
-correct_answer = str()
+def main():
+    parity_check()
 
-random_number = randint(1, 100)
 
-
-def brain_even(random_number):
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    print(f'Question: {random_number}')
-    answer = input()
-    i = 0
-    while True:
-        if random_number % 2 == 0 and answer == 'yes':
-            True
-            print('Correct!')
-            i += 1
-            if i == 3:
-                print('Congratulations, name!')
-                break
-        elif random_number % 2 != 0 and answer == 'no':
-            True
-            print('Correct!')
-            i += 1
-            if i == 3:
-                print('Congratulations, Sam!')
-                break
-        else: 
-            False 
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, name!")
-            break
-
-brain_even(random_number)
+if __name__ == '__main__':
+    main()
