c9557197f50918b83692be8f31a5061f3f04c0d8
gregory.s <73caramba@gmail.com>
2022-09-04 12:28:30 +0300

    fix#2

--- brain_games/games/arithmetic_progression.py
+++ brain_games/games/arithmetic_progression.py
@@ -11,33 +11,33 @@ def arithmetic_progression():
 
     i = 0  # Count of the game rounds
     while True:
-        first_number = randint(1, 49)
-        second_number = randint(50, 100)
+        first_number = randint(1, 100)
         n = randint(2, 10)
-        progression = range(first_number, second_number, n)
-        len_progression = len(progression)
-        index = randint(2, len_progression - 1)
-        correct_answer = progression[index]
+        count = randint(5, 10)
+        answer_index = randint(1, count)
+        question = f"{first_number}"
+        correct_answer = ""
 
-        if len_progression >= 5 and len_progression <= 10:
-            # Removing [ and ]
-            str_progression = str(list(progression))[1:-1]  # Removing [ and ]
-            # Formating question
-            question = (str_progression.replace(str(correct_answer), ".."))
-            comma = ','
-            # Removing commas and asking a question
-            print(f'Question: {question.replace(comma,"")}')
-            answer = (input('Your answer: '))  # Asking player's answer
+        for num in range(1, count):
+            first_number += n
+            if num == answer_index:
+                question = f"{question} .."
+                correct_answer = str(first_number)
+            else:
+                question = f"{question} {first_number}"
 
-            if answer == str(correct_answer):  # Player's answer is correct
-                print('Correct!')
-                i += 1
-            else:  # Player's answer is incorrect
-                print(f"'{answer}' is wrong answer ;(. "
-                      f"Correct answer was '{correct_answer}'."
-                      f"\nLet's try again, {name}!")
-                break
+        print(f"Question: {question}")
+        answer = (input('Your answer: '))  # Asking player's answer
 
-            if i == 3:
-                print(f'Congratulations, {name}!')
-                break
+        if answer == str(correct_answer):  # Player's answer is correct
+            print('Correct!')
+            i += 1
+        else:  # Player's answer is incorrect
+            print(f"'{answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
+                f"\nLet's try again, {name}!")
+            break
+
+        if i == 3:
+            print(f'Congratulations, {name}!')
+            break
